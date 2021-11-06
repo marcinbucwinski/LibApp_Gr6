d567743169743e859bb56dba26e6a594052eea39
pajakwsei <mpajak@wsei.edu.pl>
2021-11-06 22:21:23 +0100

    Added Customer and RandomBookView models, modified Random view to accept RandomBookViewModel type

---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace LibApp_Gr6.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LibApp_Gr6.Models;
+
+namespace LibApp_Gr6.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
